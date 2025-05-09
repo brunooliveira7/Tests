2a9f5e58a2679d9dbcc7dcef96093d089f950a2d
Bruno Oliveira <brunoroliveira7@gmail.com>
2025-05-09 10:51:46 -0300

    coding the first test

--- src/subtraction.test.ts
+++
@@ -1,4 +0,0 @@
-//test() - jest, nome e a função que será testada
-test("subtraction", () => {
-  console.log("test subtraction ok!");
-});
--- src/sum.test.ts
+++ src/sum.test.ts
@@ -1,4 +1,9 @@
+import  { sum } from "./server";
+
 //test() - jest, nome e a função que será testada
-test("sum", () => {
-  console.log("test sum ok!");
+test("sum of 3 + 7 must be 10", () => {
+  const result = sum(3, 7);
+  
+  //expect() - o que será testado, toBe - o que é esperado
+  expect(result).toBe(10);
 });
