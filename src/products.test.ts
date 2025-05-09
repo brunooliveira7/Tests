f940e00c0fa9a1db4087f1f30a4d31f07fc5db53
Bruno Oliveira <brunoroliveira7@gmail.com>
2025-05-09 16:52:11 -0300

    validating E2E test

--- src/products.test.ts
+++ src/products.test.ts
@@ -4,6 +4,10 @@ import { app } from "./app"; //não sobe o servidor, o server sim - defini a por
 describe("products", () => {
   it("should list product", async() => {
     const response = await request(app).get("/products");
-    console.log(response.body);
+    
+    //verifica se o status da resposta é 200 - sucesso
+    expect(response.statusCode).toBe(200);
+    //verifica o tamanho do array de produtos
+    expect(response.body).toHaveLength(3);
   });
 });
