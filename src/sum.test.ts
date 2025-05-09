bcecb847e1264cfea6c7fbeaddc7a9a1da58a4c4
Bruno Oliveira <brunoroliveira7@gmail.com>
2025-05-09 15:09:19 -0300

    using It

--- src/sum.test.ts
+++ src/sum.test.ts
@@ -3,7 +3,8 @@ import { sum } from "./server";
 //describe() - nome do grupo de testes - suite
 describe("sum", () => {
   //test() - jest, nome e a função que será testada
-  test("sum of 3 + 7 must be 10", () => {
+  //it() = test()
+  it("should do sum of 3 + 7 must be 10", () => {
     const result = sum(3, 7);
 
     //expect() - o que será testado, toBe - o que é esperado
