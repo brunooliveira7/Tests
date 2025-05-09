bdd16747efd6c687b704431dbb3de010c561db7a
Bruno Oliveira <brunoroliveira7@gmail.com>
2025-05-09 15:06:37 -0300

    grouping tests - describe

--- src/sum.test.ts
+++ src/sum.test.ts
@@ -1,9 +1,19 @@
-import  { sum } from "./server";
+import { sum } from "./server";
 
-//test() - jest, nome e a função que será testada
-test("sum of 3 + 7 must be 10", () => {
-  const result = sum(3, 7);
-  
-  //expect() - o que será testado, toBe - o que é esperado
-  expect(result).toBe(10);
+//describe() - nome do grupo de testes - suite
+describe("sum", () => {
+  //test() - jest, nome e a função que será testada
+  test("sum of 3 + 7 must be 10", () => {
+    const result = sum(3, 7);
+
+    //expect() - o que será testado, toBe - o que é esperado
+    expect(result).toBe(10);
+  });
+
+  test("sum of 2 + 2 must be 4", () => {
+    const result = sum(2, 2);
+
+    //expect() - o que será testado, toBe - o que é esperado
+    expect(result).toBe(4);
+  });
 });
